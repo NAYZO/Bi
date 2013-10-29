96f815d2934b13b1479bcb09de96b6ce6a36b6a4
nayzo <nayzo@nayzo-comp>
2013-10-29 20:35:57 +0100

    Update by Nayzo

--- src/bi/Bi.java
+++ src/bi/Bi.java
@@ -17,10 +17,6 @@ public class Bi {
      */
     public static void main(String[] args) {
         
-        // Initialisation du Timer
-        Timer timer = new Timer();
-        timer.startTimer();
-        
         // Ouverture des connexion
         Connection conn1 = ConnexionBD.openConnectionForMySql(Constante.db_trans);
         Connection conn2 = ConnexionBD.openConnectionForMySql(Constante.db_dwh);
@@ -33,15 +29,20 @@ public class Bi {
         // Remplissage des tables de la base transactionnelle
         ConnexionBD.remplirTables(conn1, 20);
         
+        // Initialisation du Timer
+        Timer timer = new Timer();
+        timer.startTimer();
+        
         // Transformation des données et remplissage de la base DWH
         Traitement tr = new Traitement(conn1, conn2);
         tr.transform();
         
-        // Fermeture des connexion
-        ConnexionBD.closeConnection(conn1);
-        ConnexionBD.closeConnection(conn2);
-        
         // Affichage du temps d'execution
         timer.showTimer();
+        
+        // Fermeture des connexion
+        ConnexionBD.closeConnection(conn1);
+        ConnexionBD.closeConnection(conn2);        
+        
     }
 }
